49e4330c693cd9fa77d3d7d046c332921de9feb6
Chouette4u <Chouette4u@users.noreply.github.com>
2015-02-23 17:07:45 -0500

    Updated for Worlds Apart
    Changed castaway names to Worlds Apart cast.  Switched from team/partner model to tribe model.

--- survivor_flair.py
+++ survivor_flair.py
@@ -19,60 +19,51 @@ sr = 'survivor'
 subreddit = r.get_subreddit(sr)
 
 individuals = [
-    'Jeremy',
-    'Val',
-    'Kelley',
-    'Dale',
-    'Drew',
-    'Alec',
-    'Julie',
-    'John',
-    'Keith',
-    'Wes',
-    'Reed',
-    'Josh',
-    'Natalie',
-    'Nadiya',
-    'Jon',
-    'Jaclyn',
-    'Missy',
-    'Baylor',
-    'Muffin'
+    'Carolyn',
+    'Dan',
+    'Hali',
+    'Jenn',
+    'Joaquin',
+    'Joe',
+    'Kelly',
+    'Lindsey',
+    'Max',
+    'Mike',
+    'Nina',
+    'Rodney',
+    'Shirin',
+    'Sierra',
+    'So',
+    'Tyler',
+    'Vince',
+    'Will'
 ]
 
-teams = [
-    'Twinnies',
-    'Dale and Kelley',
-    'Drew and Alec',
-    'Jeremy and Val',
-    'John and Julie',
-    'Josh and Reed',
-    'Jon and Jaclyn',
-    'Keith and Wes',
-    'Missy and Baylor',
-    'Natalie and Nadiya',
+tribes = [
+    'White Collar',
+    'No Collar',
+    'Blue Collar'
 ]
 
-team_membership = {
-    'Jeremy': 'Jeremy and Val',
-    'Val': 'Jeremy and Val',
-    'Kelley': 'Dale and Kelley',
-    'Dale': 'Dale and Kelley',
-    'Drew': 'Drew and Alec',
-    'Alec': 'Drew and Alec',
-    'John': 'John and Julie',
-    'Julie': 'John and Julie',
-    'Josh': 'Josh and Reed',
-    'Reed': 'Josh and Reed',
-    'Jon': 'Jon and Jaclyn',
-    'Jaclyn': 'Jon and Jaclyn',
-    'Kieth': 'Keith and Wes',
-    'Wes': 'Keith and Wes',
-    'Missy': 'Missy and Baylor',
-    'Baylor': 'Missy and Baylor',
-    'Natalie': 'Natalie and Nadiya',
-    'Nadiya': 'Natalie and Nadiya',
-    'Twinnies': 'Natalie and Nadiya'
+tribe_membership = {
+    'Carolyn': 'White Collar',
+    'Dan': 'Blue Collar',
+    'Hali': 'No Collar',
+    'Jenn': 'No Collar',
+    'Joaquin': 'White Collar',
+    'Joe': 'No Collar',
+    'Kelly': 'Blue Collar',
+    'Lindsey': 'Blue Collar',
+    'Max': 'White Collar',
+    'Mike': 'Blue Collar',
+    'Nina': 'No Collar',
+    'Rodney': 'White Collar',
+    'Shirin': 'White Collar',
+    'Sierra': 'Blue Collar',
+    'So': 'White Collar',
+    'Tyler': 'White Collar',
+    'Vince': 'No Collar',
+    'Will': 'No Collar'
 }
 
  
@@ -107,11 +98,11 @@ def normalized_css_class(input):
         return 'player'
     return 'UNDEFINED'
 
-def normalized_team(input):
-    if input in teams:
+def normalized_tribe(input):
+    if input in tribes:
         return input
-    if input in team_membership:
-        return team_membership[input]
+    if input in tribe_membership:
+        return tribe_membership[input]
     return 'UNDEFINED'
 
 def main():
@@ -119,14 +110,14 @@ def main():
     css_classes = {}
     texts = {}
     combined = {}
-    team_agg = {}
+    tribe_agg = {}
     total = 0
 
     for flair in subreddit.get_flair_list(limit = None):
         user = flair['user'] or 'UNDEFINED'
         css_class = normalized_css_class(flair['flair_css_class'])
         flair_text = flair['flair_text'] or 'UNDEFINED'
-        team = normalized_team(flair_text)
+        tribe = normalized_tribe(flair_text)
 
         if user not in users:
             users[user] = 0
@@ -134,10 +125,10 @@ def main():
             css_classes[css_class] = 0
         if flair_text not in texts:
             texts[flair_text] = 0
-        if team not in team_agg:
-            team_agg[team] = {}
-        if css_class not in team_agg[team]:
-            team_agg[team][css_class] = 0
+        if tribe not in tribe_agg:
+            tribe_agg[tribe] = {}
+        if css_class not in tribe_agg[tribe]:
+            tribe_agg[tribe][css_class] = 0
         if flair_text not in combined:
             combined[flair_text] = { }
         if css_class not in combined[flair_text]:
@@ -147,7 +138,7 @@ def main():
         css_classes[css_class] += 1 
         users[user] += 1
         combined[flair_text][css_class] += 1
-        team_agg[team][css_class] += 1
+        tribe_agg[tribe][css_class] += 1
         total += 1
 
         if total % 1000 == 0:
@@ -157,14 +148,14 @@ def main():
     # write_out(users, "users_filtered.csv", False, current_flair_text)
     # write_out(css_classes, "css_classes.csv", False, None)
     # write_out(css_classes, "css_classes_individual.csv", False, current_flair_text)
-    write_out(team_agg, "teams.csv", True, teams)
+    write_out(tribe_agg, "tribes.csv", True, tribes)
     write_out(texts, "texts.csv", False, None)
     write_out(texts, "texts_individuals.csv", False, individuals)
-    write_out(texts, "texts_teams.csv", False, teams)
+    write_out(texts, "texts_tribes.csv", False, tribes)
     write_out(combined, "combined.csv", True, None)
     write_out(combined, "combined_individuals.csv", True, individuals)
-    write_out(combined, "combined_teams.csv", True, teams)
-    write_out(combined, "combined_season.csv", True, individuals + teams)
+    write_out(combined, "combined_tribes.csv", True, tribes)
+    write_out(combined, "combined_season.csv", True, individuals + tribes)
  
 if __name__ == "__main__":
     main()
